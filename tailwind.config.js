72f0552f60fde95177968ad7a6cac03ac6bdc097
Dmaul0906 <dimasmaulana0906@gmail.com>
2021-11-27 23:30:06 +0700

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
